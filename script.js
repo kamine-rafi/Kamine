fbea6b6d335bf6a157dade15bca0fd8cc52be1c8
kamine-rafi <chowdhuryrafi159@gmail.com>
2026-07-07 03:22:18 +0600

    Add script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("Kamine AI Started 🚀");
+
+    const button = document.querySelector("button");
+
+    if (button) {
+        button.addEventListener("click", () => {
+            alert("Welcome to Kamine AI!\nOwner Mode will be added soon.");
+        });
+    }
+});
