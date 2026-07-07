c48734c36b860183a919cd1f90639ae9ac8a4ca6
kamine-rafi <chowdhuryrafi159@gmail.com>
2026-07-07 22:39:41 +0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,16 @@
+const KAMINE_CONFIG = {
+    name: "Kamine",
+    owner: "KM Rafi Chowdhury",
+    version: "1.0",
+
+    assistantMode: "Personal AI Assistant",
+
+    features: {
+        voice: true,
+        memory: true,
+        image: true,
+        documents: true
+    }
+};
+
+console.log("Kamine AI Loaded");
