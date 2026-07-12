d717d4a6c6f811b81fb763ab54cb2acbae3ce0b4
kamine-rafi <chowdhuryrafi159@gmail.com>
2026-07-12 22:50:58 +0600

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,18 +1,6 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
-
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-
-export async function loginWithGoogle() {
-    try {
-        const result = await signInWithPopup(auth, provider);
-        return result.user;
-    } catch (error) {
-        console.error("Login Error:", error);
-        return null;
+export function checkMode(user) {
+    if (user && user.email === "rafi@owner.com") {
+        return "OWNER_MODE";
     }
-}
-
-export function logoutUser() {
-    signOut(auth);
+    return "PUBLIC_MODE";
 }
