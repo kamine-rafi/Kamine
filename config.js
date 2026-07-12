509d63251a61bfaaab4ea4f2bbc2d3cff9e58477
kamine-rafi <chowdhuryrafi159@gmail.com>
2026-07-12 22:49:41 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,15 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-auth.js";
-
+import { initializeApp } from "firebase/app";
 const firebaseConfig = {
   apiKey: "AIzaSyBPf2yQQ1ixTG7y3RBqE5aJj_HcQ3OaoC8",
   authDomain: "kanine-rafi.firebaseapp.com",
   projectId: "kanine-rafi",
   storageBucket: "kanine-rafi.firebasestorage.app",
   messagingSenderId: "219986114815",
-  appId: "1:219986114815:web:b1b549b6fdd991573434ca",
-  measurementId: "G-GTFQ5YFLEF"
+  appId: "1:219986114815:web:b1b549b6fdd991573434ca"
 };
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+export const app = initializeApp(firebaseConfig);
